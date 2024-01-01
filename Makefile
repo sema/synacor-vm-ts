fab5ab05d562a94bebe9dded53fa4f352b79192a
Casper Svenning Jensen <casper@svenningjensen.dk>
2024-01-01 20:48:06 +0100

    Support running different commands, including prettyprint and execute

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-run:
-	bun run src/main.ts challenge/challenge.bin
+prettyprint:
+	bun run src/main.ts prettyprint challenge/challenge.bin
+
+execute:
+	bun run src/main.ts execute challenge/challenge.bin
 
 test:
 	bun test
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import {parse} from './parse'
 import {prettyPrint} from './prettyprinter'
 
-if (process.argv.length != 3) {
-    console.log("path to challenge.bin not provided")
+if (process.argv.length != 4) {
+    console.log("usage: <execute|prettyprint> <path-to-challenge.bin>")
     process.exit(1)
 }
-const challenge_bin_path = process.argv[2]
+const command = process.argv[2]
+const challenge_bin_path = process.argv[3]
 
 let tokens = parse(challenge_bin_path)
 console.log(`Parsed ${tokens.length} tokens from ${challenge_bin_path}`)
 
-prettyPrint(tokens)
+switch (command) {
+case "execute":
+    throw new Error("execute not implemented yet")
+    break
+case "prettyprint":
+    prettyPrint(tokens)
+    break
+default:
+    throw new Error("unknown command, expected either execute or prettyprint")
+}
